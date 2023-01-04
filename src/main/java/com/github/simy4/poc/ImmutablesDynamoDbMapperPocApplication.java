e93727f4642405337ae31e413c2f97be00e15f53
SimY4 <alex.simkin@outlook.com>
2023-01-04 14:36:50 +1100

    Remove AWS spring. Start using test containers.

--- src/main/java/com/github/simy4/poc/ImmutablesDynamoDbMapperPocApplication.java
+++ src/main/java/com/github/simy4/poc/ImmutablesDynamoDbMapperPocApplication.java
@@ -1,21 +1,22 @@
 package com.github.simy4.poc;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.regions.AwsRegionProvider;
+import com.amazonaws.regions.DefaultAwsRegionProviderChain;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsync;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
 import com.amazonaws.services.dynamodbv2.util.TableUtils;
 import com.github.simy4.poc.model.ModifiableEntity;
-import io.awspring.cloud.core.region.RegionProvider;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.core.env.Environment;
 
 /** Starting point of this application. */
@@ -29,33 +30,27 @@ public class ImmutablesDynamoDbMapperPocApplication {
   // AWS v1
 
   @Bean
-  public AmazonDynamoDB dynamoDB(
-      AWSCredentialsProvider awsCredentialsProvider,
-      @Value("${aws.local.endpoint:#{null}}") String localEndpoint,
-      RegionProvider regionProvider) {
-    var builder = AmazonDynamoDBClientBuilder.standard().withCredentials(awsCredentialsProvider);
-    builder =
-        null == localEndpoint
-            ? builder.withRegion(regionProvider.getRegion().getName())
-            : builder.withEndpointConfiguration(
-                new AwsClientBuilder.EndpointConfiguration(
-                    localEndpoint, regionProvider.getRegion().getName()));
-    return builder.build();
+  public AWSCredentialsProvider awsCredentialsProvider() {
+    return DefaultAWSCredentialsProviderChain.getInstance();
   }
 
   @Bean
-  public ApplicationRunner dynamoDBInitializer(
-      AmazonDynamoDB dynamoDB, DynamoDBMapper dynamoDBMapper) {
-    return args -> {
-      var request = dynamoDBMapper.generateCreateTableRequest(ModifiableEntity.class);
-      TableUtils.createTableIfNotExists(
-          dynamoDB, request.withProvisionedThroughput(new ProvisionedThroughput(2L, 2L)));
-      TableUtils.waitUntilActive(dynamoDB, request.getTableName());
-    };
+  public AwsRegionProvider awsRegionProvider() {
+    return new DefaultAwsRegionProviderChain();
+  }
+
+  @Lazy
+  @Bean
+  public AmazonDynamoDBAsync dynamoDB(
+      AWSCredentialsProvider awsCredentialsProvider, AwsRegionProvider regionProvider) {
+    return AmazonDynamoDBAsyncClientBuilder.standard()
+        .withCredentials(awsCredentialsProvider)
+        .withRegion(regionProvider.getRegion())
+        .build();
   }
 
   @Bean
-  public DynamoDBMapper dynamoDBMapper(AmazonDynamoDB dynamoDB, Environment env) {
+  public DynamoDBMapper dynamoDBMapper(AmazonDynamoDBAsync dynamoDB, Environment env) {
     var builder = DynamoDBMapperConfig.builder();
     builder.setTableNameResolver(
         (clazz, config) -> {
@@ -63,5 +58,16 @@ public class ImmutablesDynamoDbMapperPocApplication {
           return env.resolvePlaceholders(dynamoDBTable.tableName());
         });
     return new DynamoDBMapper(dynamoDB, builder.build());
+  }
+
+  @Bean
+  public ApplicationRunner dynamoDBInitializer(
+      AmazonDynamoDBAsync dynamoDB, DynamoDBMapper dynamoDBMapper) {
+    return args -> {
+      var request = dynamoDBMapper.generateCreateTableRequest(ModifiableEntity.class);
+      TableUtils.createTableIfNotExists(
+          dynamoDB, request.withProvisionedThroughput(new ProvisionedThroughput(2L, 2L)));
+      TableUtils.waitUntilActive(dynamoDB, request.getTableName());
+    };
   }
 }
