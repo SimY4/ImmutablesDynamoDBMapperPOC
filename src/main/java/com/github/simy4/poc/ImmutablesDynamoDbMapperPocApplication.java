da20fbaaa9438e496d80c8ff79f0d189ac50c6cc
SimY4 <alex.simkin@outlook.com>
2023-05-23 15:56:35 +1000

    Make application runnable locally.

--- src/main/java/com/github/simy4/poc/ImmutablesDynamoDbMapperPocApplication.java
+++ src/main/java/com/github/simy4/poc/ImmutablesDynamoDbMapperPocApplication.java
@@ -1,7 +1,11 @@
 package com.github.simy4.poc;
 
+import com.amazonaws.SdkClientException;
 import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.AwsRegionProvider;
 import com.amazonaws.regions.DefaultAwsRegionProviderChain;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsync;
@@ -12,11 +16,13 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
 import com.amazonaws.services.dynamodbv2.util.TableUtils;
 import com.github.simy4.poc.model.ModifiableEntity;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 
 /** Starting point of this application. */
@@ -30,24 +36,43 @@ public class ImmutablesDynamoDbMapperPocApplication {
   // AWS v1
 
   @Bean
-  public AWSCredentialsProvider awsCredentialsProvider() {
-    return DefaultAWSCredentialsProviderChain.getInstance();
+  public AWSCredentialsProvider awsCredentialsProvider(
+      @Value("${spring.cloud.aws.credentials.access-key:#{null}}") String staticAccessKey,
+      @Value("${spring.cloud.aws.credentials.secret-key:#{null}}") String staticSecretKey) {
+    return null == staticAccessKey || null == staticSecretKey
+        ? DefaultAWSCredentialsProviderChain.getInstance()
+        : new AWSStaticCredentialsProvider(
+            new BasicAWSCredentials(staticAccessKey, staticSecretKey));
   }
 
   @Lazy
   @Bean
-  public AwsRegionProvider awsRegionProvider() {
-    return new DefaultAwsRegionProviderChain();
+  public AwsRegionProvider awsRegionProvider(
+      @Value("${spring.cloud.aws.region.static:#{null}}") String staticRegion) {
+    return null == staticRegion
+        ? new DefaultAwsRegionProviderChain()
+        : new AwsRegionProvider() {
+          @Override
+          public String getRegion() throws SdkClientException {
+            return staticRegion;
+          }
+        };
   }
 
   @Lazy
   @Bean(destroyMethod = "shutdown")
   public AmazonDynamoDBAsync dynamoDB(
-      AWSCredentialsProvider awsCredentialsProvider, AwsRegionProvider regionProvider) {
-    return AmazonDynamoDBAsyncClientBuilder.standard()
-        .withCredentials(awsCredentialsProvider)
-        .withRegion(regionProvider.getRegion())
-        .build();
+      AWSCredentialsProvider awsCredentialsProvider,
+      AwsRegionProvider regionProvider,
+      @Value("${spring.cloud.aws.dynamodb.endpoint:#{null}}") String endpoint) {
+    AmazonDynamoDBAsyncClientBuilder builder =
+        AmazonDynamoDBAsyncClientBuilder.standard().withCredentials(awsCredentialsProvider);
+    builder =
+        null == endpoint
+            ? builder.withRegion(regionProvider.getRegion())
+            : builder.withEndpointConfiguration(
+                new AwsClientBuilder.EndpointConfiguration(endpoint, regionProvider.getRegion()));
+    return builder.build();
   }
 
   @Bean
@@ -62,6 +87,7 @@ public class ImmutablesDynamoDbMapperPocApplication {
   }
 
   @Bean
+  @Profile("local")
   public ApplicationRunner dynamoDBInitializer(
       AmazonDynamoDBAsync dynamoDB, DynamoDBMapper dynamoDBMapper) {
     return args -> {
