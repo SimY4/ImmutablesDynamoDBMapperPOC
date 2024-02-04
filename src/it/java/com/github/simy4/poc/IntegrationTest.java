0fef5cadebd22d1e354082e7c8e324d2a8d0429d
SimY4 <alex.simkin@outlook.com>
2024-02-04 14:04:46 +1000

    Dependabot for github actions.

--- src/it/java/com/github/simy4/poc/IntegrationTest.java
+++ src/it/java/com/github/simy4/poc/IntegrationTest.java
@@ -15,7 +15,7 @@ import org.testcontainers.utility.DockerImageName;
 public abstract class IntegrationTest {
   @Container
   static final LocalStackContainer localstack =
-      new LocalStackContainer(DockerImageName.parse("localstack/localstack:3.0.1"))
+      new LocalStackContainer(DockerImageName.parse("localstack/localstack:3.1.0"))
           .withServices(LocalStackContainer.Service.DYNAMODB);
 
   @DynamicPropertySource
