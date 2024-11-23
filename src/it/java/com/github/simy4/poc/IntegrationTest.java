53c5aa0d6568130ec186583200018f4244671ace
SimY4 <alex.simkin@outlook.com>
2024-11-23 18:30:56 +1000

    localstack 4.

--- src/it/java/com/github/simy4/poc/IntegrationTest.java
+++ src/it/java/com/github/simy4/poc/IntegrationTest.java
@@ -15,7 +15,7 @@ import org.testcontainers.utility.TestcontainersConfiguration;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class IntegrationTest {
   static final LocalStackContainer localstack =
-      new LocalStackContainer(DockerImageName.parse("localstack/localstack:3.7.1"))
+      new LocalStackContainer(DockerImageName.parse("localstack/localstack:4.0.1"))
           .withServices(LocalStackContainer.Service.DYNAMODB)
           .withReuse(true);
 
