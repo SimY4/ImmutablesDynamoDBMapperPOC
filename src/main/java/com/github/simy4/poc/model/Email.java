d553975488d38dabeeab62f7aaa89188bc51b11e
SimY4 <alex.simkin@outlook.com>
2025-10-20 18:13:46 +1100

    Spotless.

--- src/main/java/com/github/simy4/poc/model/Email.java
+++ src/main/java/com/github/simy4/poc/model/Email.java
@@ -6,19 +6,16 @@ import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 
 @NullMarked
 @Value.Builder
-public record Email(@jakarta.validation.constraints.Email String email, boolean verified, boolean primary) {
+public record Email(
+    @jakarta.validation.constraints.Email String email, boolean verified, boolean primary) {
   static TableSchema<Email> schema() {
     return TableSchema.builder(Email.class, EmailBuilder.class)
         .newItemBuilder(EmailBuilder::new, EmailBuilder::build)
         .addAttribute(
-            String.class,
-            a -> a.name("email").getter(Email::email).setter(EmailBuilder::email))
+            String.class, a -> a.name("email").getter(Email::email).setter(EmailBuilder::email))
         .addAttribute(
             Boolean.class,
-            a ->
-                a.name("verified")
-                    .getter(Email::verified)
-                    .setter(EmailBuilder::verified))
+            a -> a.name("verified").getter(Email::verified).setter(EmailBuilder::verified))
         .addAttribute(
             Boolean.class,
             a -> a.name("primary").getter(Email::primary).setter(EmailBuilder::primary))
